fd760a31d864f1bf60980e5013a7e56dfe32fb0b
TAISEI0704 <132648502+TAISEI0704@users.noreply.github.com>
2023-10-18 13:20:02 +0900

    dashbord変更

---
+++ resources/views/components/task-list.blade.php
@@ -0,0 +1,42 @@
+<div class="flex flex-col">
+    <div class="-m-1.5 overflow-x-auto">
+        <div class="p-1.5 min-w-full inline-block align-middle">
+            <div class="border rounded-lg shadow overflow-hidden dark:border-gray-700 dark:shadow-gray-900">
+                <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                    <thead class="bg-gray-50 dark:bg-gray-700">
+                        <tr>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase dark:text-gray-400">No</th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase dark:text-gray-400">Name</th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase dark:text-gray-400">Done</th>
+                        </tr>
+                    </thead>
+                    <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                                <a class="text-blue-500 hover:text-blue-700" href="#">1</a>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">Test Task 1</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">o</td>
+                        </tr>
+
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                                <a class="text-blue-500 hover:text-blue-700" href="#">2</a>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">Test Task 2</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">o</td>
+                        </tr>
+
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                                <a class="text-blue-500 hover:text-blue-700" href="#">3</a>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">Test Task 3</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">o</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
